57d9e87aaa59babff9813a04bad62c164c33fc46
nerfthisdev <353054@niuitmo.ru>
2025-05-15 18:42:00 +0300

    feat: add main file with entry point and input of generations

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/nerfthisdev-itmo/mo-lab6/internal/genetic"
+)
+
+func main() {
+	var n int
+
+	fmt.Println("Please input the number of generations: ")
+	fmt.Scan(&n)
+
+	population := genetic.GeneratePopulation(4)
+
+	for i := range n {
+		fmt.Printf("GEN %d\n", i+1)
+		fmt.Println("------------------------------------")
+		genetic.Evolve(population)
+		fmt.Println("------------------------------------")
+	}
 }
